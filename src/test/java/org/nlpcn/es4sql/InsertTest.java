95c5300cf5905affe4e0a178104b47d8868e7f58
liushaobo <lshb1986@qq.com>
2014-08-25 14:21:12 +0800

    test

--- src/test/java/org/nlpcn/es4sql/InsertTest.java
+++ src/test/java/org/nlpcn/es4sql/InsertTest.java
@@ -2,7 +2,6 @@ package org.nlpcn.es4sql;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -10,21 +9,24 @@ import org.elasticsearch.ElasticsearchException;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.nlpcn.commons.lang.util.FileFinder;
 
 import com.alibaba.fastjson.JSONObject;
 
 public class InsertTest {
 	public static void main(String[] args) throws ElasticsearchException, IOException {
 		Client client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("127.0.0.1", 9300));
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("/Users/ansj/Documents/temp/gather_data.txt")));
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(FileFinder.find("accounts.json"))));
 		String temp = null;
 		int i = 0;
 		while ((temp = br.readLine()) != null) {
 			JSONObject job = JSONObject.parseObject(temp) ;
+			if (job.containsKey("_id")) {
+			    continue;
+			}
 			try {
-				client.prepareIndex().setIndex("doc").setType("news").setId(job.remove("_id").toString()).setSource(job).execute().actionGet();
+				client.prepareIndex().setIndex("bank").setType("accounts").setId(job.getString("account_number")).setSource(job).execute().actionGet();
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
