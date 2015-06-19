bc70defdb8d2bc7a5ed76edf3c78a666b87456ce
Jake Heimbouch <jheimbouch@redstonecontentsolutions.com>
2015-06-19 09:59:00 -0500

    * Can specify aggregation fields through GET using "&aggs=comma,delimited,field,names" * Can specify aggregation fields through POST by adding "<aggs>comma,delimited,field,names</aggs>" * You can also add sub aggregates for an aggregate by using a colon - e.g. "Firstname:age"

--- src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
+++ src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
@@ -13,6 +13,10 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.*;
 import org.elasticsearch.rest.action.support.RestBuilderListener;
 import org.elasticsearch.rest.action.support.RestStatusToXContentListener;
+import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 import org.nlpcn.es4sql.SearchDao;
 import org.nlpcn.es4sql.query.explain.ExplainManager;
 
@@ -31,16 +35,36 @@ public class RestSqlAction extends BaseRestHandler {
 
 	@Override
 	protected void handleRequest(RestRequest request, RestChannel channel, final Client client) throws Exception {
+		final String aggsStart = "<aggs>";
+		final String aggsEnd = "</aggs>";
+
 
 		String sql = request.param("sql");
+		String aggs = request.param("aggs");
 
 		if (sql == null) {
 			sql = request.content().toUtf8();
+			int startIndex = sql.indexOf(aggsStart);
+			int endIndex = sql.indexOf(aggsEnd);
+
+			// If <aggs></aggs> is properly included in the Post Body
+			if (startIndex >= 0 && endIndex >= 0) {
+				// Grab the the aggs element
+				String aggElement = sql.substring(startIndex, endIndex + aggsEnd.length());
+				// Remove it from the sql param (body)
+				sql = sql.replaceAll(aggElement, "");
+				// Assign aggs to the value of the element with the the tags
+				aggs = aggElement.substring(aggsStart.length(), aggElement.length() - aggsEnd.length());
+			}
 		}
 
 		SearchDao searchDao = new SearchDao(client);
 		ActionRequestBuilder actionRequestBuilder = searchDao.explain(sql);
+
 		ActionRequest actionRequest = actionRequestBuilder.request();
+
+		// Add our aggregations to the action request builder if necessary
+		handleAggs(aggs, actionRequestBuilder);
 
 		// TODO add unittests to explain. (rest level?)
 		if (request.path().endsWith("/_explain")) {
@@ -49,6 +73,36 @@ public class RestSqlAction extends BaseRestHandler {
 			channel.sendResponse(bytesRestResponse);
 		} else {
 			new ActionRequestExecuter(actionRequest, channel, client).execute();
+		}
+	}
+
+	private void handleAggs(String aggs, ActionRequestBuilder actionRequestBuilder) {
+		// If the aggs parameter isn't null or empty
+		if (aggs != null && aggs.trim().length() > 0) {
+			// Get a reference to Action Request Builder
+			SearchRequestBuilder b = (SearchRequestBuilder) actionRequestBuilder;
+
+			// Split the comma delimited field names
+			String[] arAggs = aggs.split(",");
+			for (String strAgg : arAggs) {
+				strAgg = strAgg.trim();
+
+				// Split again on colons for sub aggregates
+				String[] arSubAggs = strAgg.split(":");
+				TermsBuilder objOriginalTerms = null;
+				for (String strSubAgg: arSubAggs) {
+					if (objOriginalTerms == null) {
+						objOriginalTerms = AggregationBuilders.terms(strSubAgg).field(strSubAgg).size(0).order(Terms.Order.term(true));
+						// Add our aggregation to the actionRequestBuilder
+						b.addAggregation(objOriginalTerms);
+					}
+					else {
+						TermsBuilder objSubTerms = AggregationBuilders.terms(strSubAgg).field(strSubAgg).size(0).order(Terms.Order.term(true));
+						objOriginalTerms.subAggregation(objSubTerms);
+						objOriginalTerms = objSubTerms;
+					}
+				}
+			}
 		}
 	}
 }
