3aaac10c1592ccb30dff1b03fc12b7485a3feb0e
Caleb Ott <cott@redstonecontentsolutions.com>
2015-07-31 08:30:34 -0500

    Removed some unnecessary imports

--- src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
+++ src/main/java/org/elasticsearch/plugin/nlpcn/RestSqlAction.java
@@ -2,25 +2,12 @@ package org.elasticsearch.plugin.nlpcn;
 
 import org.elasticsearch.action.ActionRequest;
 import org.elasticsearch.action.ActionRequestBuilder;
-import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchRequestBuilder;
-import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.common.inject.Inject;
-import org.elasticsearch.common.io.stream.BytesStreamOutput;
-import org.elasticsearch.common.io.stream.StreamOutput;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.rest.*;
-import org.elasticsearch.rest.action.support.RestBuilderListener;
-import org.elasticsearch.rest.action.support.RestStatusToXContentListener;
-import org.elasticsearch.search.aggregations.AbstractAggregationBuilder;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.bucket.terms.Terms;
-import org.elasticsearch.search.aggregations.bucket.terms.TermsBuilder;
 import org.nlpcn.es4sql.SearchDao;
 import org.nlpcn.es4sql.query.explain.ExplainManager;
-
-import java.io.FileOutputStream;
 
 public class RestSqlAction extends BaseRestHandler {
 
