cdf13b5e65abc050f32a8c081ac340f1225872bc
WilliamZhu <allwefantasy@gmail.com>
2016-08-31 11:05:32 +0800

    add jdbc support

---
+++ src/main/java/com/alibaba/druid/pool/ElasticSearchResultSetMetaDataBase.java
@@ -0,0 +1,23 @@
+package com.alibaba.druid.pool;
+
+import com.alibaba.druid.util.jdbc.ResultSetBase;
+import com.alibaba.druid.util.jdbc.ResultSetMetaDataBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by allwefantasy on 8/31/16.
+ */
+public class ElasticSearchResultSetMetaDataBase extends ResultSetMetaDataBase {
+    private final List<ColumnMetaData> columns = new ArrayList<ColumnMetaData>();
+
+    public ElasticSearchResultSetMetaDataBase(List<String> headers) {
+        for(String column:headers){
+            ColumnMetaData columnMetaData = new ColumnMetaData();
+            columnMetaData.setColumnLabel(column);
+            columnMetaData.setColumnName(column);
+            columns.add(columnMetaData);
+        }
+    }
+}
--- src/test/java/org/nlpcn/es4sql/JDBCTests.java
+++ src/test/java/org/nlpcn/es4sql/JDBCTests.java
@@ -1,20 +1,11 @@
 package org.nlpcn.es4sql;
 
 
-import com.google.common.collect.Maps;
-import org.apache.calcite.adapter.java.ReflectiveSchema;
-import org.apache.calcite.jdbc.CalciteConnection;
-import org.apache.calcite.schema.SchemaPlus;
-import org.junit.Test;
-import org.nlpcn.es4sql.exception.SqlParseException;
-import org.nlpcn.es4sql.jdbc.ElasticsearchSchema;
-import org.nlpcn.es4sql.jdbc.ElasticsearchSchemaFactory;
+import com.alibaba.druid.pool.DruidDataSource;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
+import com.alibaba.druid.pool.ElasticSearchDruidDataSourceFactory;
+import org.junit.Test;
 import java.sql.*;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -22,34 +13,21 @@ import java.util.Properties;
  */
 public class JDBCTests {
     @Test
-    public void testJDBC() throws IOException, SqlParseException, NoSuchMethodException, IllegalAccessException, SQLException, InvocationTargetException, ClassNotFoundException {
-        Class.forName("org.apache.calcite.jdbc.Driver");
-        Properties info = new Properties();
-        info.setProperty("lex", "JAVA");
-        Connection connection = DriverManager.getConnection("jdbc:calcite://127.0.0.1:9200", info);
-        CalciteConnection calciteConnection =
-                connection.unwrap(CalciteConnection.class);
-
-        SchemaPlus rootSchema = calciteConnection.getRootSchema();
-
-        Map<String, Integer> cordinals = new HashMap<>();
-        cordinals.put("127.0.0.1", 9300);
-
-        Map<String, String> userConfig = new HashMap<String, String>();
-
-        rootSchema.add("cluster1", new ElasticsearchSchema(cordinals, userConfig, "twitter2"));
-
-
-        Statement statement = calciteConnection.createStatement();
-        ResultSet resultSet = statement.executeQuery(
-                "select * from cluster1.twitter2  limit 10");
+    public void testJDBC() throws Exception {
+        Properties properties = new Properties();
+        properties.put("url", "jdbc:elasticsearch://127.0.0.1:9300/twitter2");
+        DruidDataSource dds = (DruidDataSource) ElasticSearchDruidDataSourceFactory.createDataSource(properties);
+        dds.setInitialSize(1);
+        Connection connection = dds.getConnection();
+        PreparedStatement ps = connection.prepareStatement("SELECT split(trim(concat_ws('dd',newtype,num_d)),'dd')[0] as nt from  twitter2");
+        ResultSet resultSet = ps.executeQuery();
         while (resultSet.next()) {
-            System.out.println(resultSet.getString(""));
+            System.out.println(resultSet.getString("nt"));
         }
 
-        resultSet.close();
-        statement.close();
+        ps.close();
         connection.close();
+        dds.close();
     }
 
 }
