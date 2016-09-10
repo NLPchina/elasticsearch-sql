6334975795f3a3a67bc875d70aa8fc6116439a9e
eliran moyal <eliran.moyal1@gmail.com>
2016-09-10 12:27:11 +0300

    support minus queries

---
+++ src/main/java/org/elasticsearch/plugin/nlpcn/ComperableHitResult.java
@@ -0,0 +1,7 @@
+package org.elasticsearch.plugin.nlpcn;
+
+/**
+ * Created by Eliran on 9/9/2016.
+ */
+public class ComperableHitResult {
+}
---
+++ src/main/java/org/elasticsearch/plugin/nlpcn/MinusExecutor.java
@@ -0,0 +1,7 @@
+package org.elasticsearch.plugin.nlpcn;
+
+/**
+ * Created by Eliran on 26/8/2016.
+ */
+public class MinusExecutor {
+}
