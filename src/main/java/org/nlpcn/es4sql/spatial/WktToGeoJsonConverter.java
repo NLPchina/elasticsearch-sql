674b90bff66f3029e0acec9b4b87db444e9c350e
石源 <shiy@infcn.com.cn>
2017-06-15 17:25:46 +0800

    fix bug #442

--- src/main/java/org/nlpcn/es4sql/spatial/WktToGeoJsonConverter.java
+++ src/main/java/org/nlpcn/es4sql/spatial/WktToGeoJsonConverter.java
@@ -139,7 +139,7 @@ public class WktToGeoJsonConverter {
     }
 
     private static String extractCoordinateFromPoint(String point) {
-        String pointPattern = "(\\s*)([0-9\\.]+)(\\s*)([0-9\\.]+)(\\s*)";
+        String pointPattern = "(\\s*)([0-9\\.-]+)(\\s*)([0-9\\.-]+)(\\s*)";
         return point.replaceAll(pointPattern,"[$2,$4]");
     }
 
