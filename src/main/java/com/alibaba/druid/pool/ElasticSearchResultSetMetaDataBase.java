4534f7c88f6e9c1f9527010f06fc1b1612e7ad7d
sishu@dtstack.com <sishu@dtstack.com>
2017-07-25 15:23:06 +0800

    ysq

--- src/main/java/com/alibaba/druid/pool/ElasticSearchResultSetMetaDataBase.java
+++ src/main/java/com/alibaba/druid/pool/ElasticSearchResultSetMetaDataBase.java
@@ -6,10 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by allwefantasy on 8/31/16.
+ * Created by allwefantasy on 8/31/16.fy
+ * modify by sishu.yss
  */
 public class ElasticSearchResultSetMetaDataBase extends ResultSetMetaDataBase {
-    private final List<ColumnMetaData> columns = new ArrayList<ColumnMetaData>();
+
+    protected final List<ColumnMetaData> columns = new ArrayList<ColumnMetaData>();
 
     public ElasticSearchResultSetMetaDataBase(List<String> headers) {
         for(String column:headers){
@@ -20,8 +22,147 @@ public class ElasticSearchResultSetMetaDataBase extends ResultSetMetaDataBase {
         }
     }
 
+
+    public List<ColumnMetaData> getColumns() {
+        return columns;
+    }
+
+    public int findColumn(String columnName) throws SQLException {
+        for (int i = 0; i < columns.size(); ++i) {
+            ColumnMetaData column = columns.get(i);
+            if (column.getColumnName().equals(columnName)) {
+                return i + 1;
+            }
+        }
+
+        throw new SQLException("column '" + columnName + "' not found.");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        if (isWrapperFor(iface)) {
+            return (T) this;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        if (iface == null) {
+            return false;
+        }
+
+        return iface.isAssignableFrom(this.getClass());
+
+    }
+
     @Override
     public int getColumnCount() throws SQLException {
         return columns.size();
+    }
+
+    @Override
+    public boolean isAutoIncrement(int column) throws SQLException {
+        return getColumn(column).isAutoIncrement();
+    }
+
+    @Override
+    public boolean isCaseSensitive(int column) throws SQLException {
+        return getColumn(column).isCaseSensitive();
+    }
+
+    @Override
+    public boolean isSearchable(int column) throws SQLException {
+        return getColumn(column).isSearchable();
+    }
+
+    @Override
+    public boolean isCurrency(int column) throws SQLException {
+        return getColumn(column).isCurrency();
+    }
+
+    @Override
+    public int isNullable(int column) throws SQLException {
+        return getColumn(column).getNullable();
+    }
+
+    @Override
+    public boolean isSigned(int column) throws SQLException {
+        return getColumn(column).isSigned();
+    }
+
+    @Override
+    public int getColumnDisplaySize(int column) throws SQLException {
+        return getColumn(column).getColumnDisplaySize();
+    }
+
+    @Override
+    public String getColumnLabel(int column) throws SQLException {
+        return getColumn(column).getColumnLabel();
+    }
+
+    public ColumnMetaData getColumn(int column) {
+        return columns.get(column - 1);
+    }
+
+    @Override
+    public String getColumnName(int column) throws SQLException {
+        return getColumn(column).getColumnName();
+    }
+
+    @Override
+    public String getSchemaName(int column) throws SQLException {
+        return getColumn(column).getSchemaName();
+    }
+
+    @Override
+    public int getPrecision(int column) throws SQLException {
+        return getColumn(column).getPrecision();
+    }
+
+    @Override
+    public int getScale(int column) throws SQLException {
+        return getColumn(column).getScale();
+    }
+
+    @Override
+    public String getTableName(int column) throws SQLException {
+        return getColumn(column).getTableName();
+    }
+
+    @Override
+    public String getCatalogName(int column) throws SQLException {
+        return getColumn(column).getCatalogName();
+    }
+
+    @Override
+    public int getColumnType(int column) throws SQLException {
+        return getColumn(column).getColumnType();
+    }
+
+    @Override
+    public String getColumnTypeName(int column) throws SQLException {
+        return getColumn(column).getColumnTypeName();
+    }
+
+    @Override
+    public boolean isReadOnly(int column) throws SQLException {
+        return getColumn(column).isReadOnly();
+    }
+
+    @Override
+    public boolean isWritable(int column) throws SQLException {
+        return getColumn(column).isWritable();
+    }
+
+    @Override
+    public boolean isDefinitelyWritable(int column) throws SQLException {
+        return getColumn(column).isDefinitelyWritable();
+    }
+
+    @Override
+    public String getColumnClassName(int column) throws SQLException {
+        return getColumn(column).getColumnClassName();
     }
 }
