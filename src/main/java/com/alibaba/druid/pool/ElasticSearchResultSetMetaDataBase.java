2f412fe7533bc26206f662084b929b4526c070fc
pengxd <pengxd@szhcf.com.cn>
2017-08-05 14:37:21 +0800

    让ElasticSearchResultSetMetaDataBase可以获取到列的metadata信息

--- src/main/java/com/alibaba/druid/pool/ElasticSearchResultSetMetaDataBase.java
+++ src/main/java/com/alibaba/druid/pool/ElasticSearchResultSetMetaDataBase.java
@@ -1,15 +1,16 @@
 package com.alibaba.druid.pool;
 
-import com.alibaba.druid.util.jdbc.ResultSetBase;
-import com.alibaba.druid.util.jdbc.ResultSetMetaDataBase;
-
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.alibaba.druid.util.jdbc.ResultSetMetaDataBase;
 
 /**
  * Created by allwefantasy on 8/31/16.
  */
 public class ElasticSearchResultSetMetaDataBase extends ResultSetMetaDataBase {
+	
     private final List<ColumnMetaData> columns = new ArrayList<ColumnMetaData>();
 
     public ElasticSearchResultSetMetaDataBase(List<String> headers) {
@@ -20,4 +21,16 @@ public class ElasticSearchResultSetMetaDataBase extends ResultSetMetaDataBase {
             columns.add(columnMetaData);
         }
     }
+    
+    @Override
+    public List<ColumnMetaData> getColumns() {
+        return columns;
+    }
+    
+    @Override
+    public int getColumnCount() throws SQLException {
+        return columns.size();
+    }
+    
+    
 }
