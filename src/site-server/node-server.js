5c9a38c27c7a8b23890f14dc79355a753d2d4cfe
wangjinlong3 <wangjinlong3@360.cn>
2021-12-02 11:00:04 +0800

    fix: path error in docker

--- src/site-server/node-server.js
+++ src/site-server/node-server.js
@@ -1,10 +1,11 @@
 var express = require('express');
+const path = require('path');
 var app = express();
-app.use(express.static('../_site'));
+app.use(express.static(path.join(__dirname, '../_site')));
 
 app.get('/', function (req, res) {
-    res.sendFile("../_site/" + "index.html" );
+    res.sendFile(path.join(__dirname, '../_site/index.html'));
 })
 var fs = require('fs');
-var siteConfiguration = JSON.parse(fs.readFileSync('site_configuration.json', 'utf8'));
-var server = app.listen(siteConfiguration.port)
+var siteConfiguration = JSON.parse(fs.readFileSync(path.join(__dirname, './site_configuration.json'), 'utf8'));
+var server = app.listen(siteConfiguration.port);
